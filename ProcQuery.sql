55e157acf7c7b56ea051971c2c04df089a10c26a
iolega <73232366+liolegal@users.noreply.github.com>
2022-12-21 13:54:10 +0300

    6 Lab

---
+++ ProcQuery.sql
@@ -0,0 +1,134 @@
+﻿Use MusicService
+
+GO
+CREATE PROC AddAccount 
+	@email VARCHAR(100),
+	@password VARCHAR(30),
+	@roleTitle VARCHAR(50) 
+AS
+	INSERT INTO Accounts(Email, Password, Role_id)
+	VALUES (
+			@email, 
+			@password, 
+			(SELECT Roles.Id FROM Roles WHERE Roles.Title = @roleTitle)
+		   )  
+GO
+
+CREATE PROC AddVisitor
+
+	@name VARCHAR(200),
+	@nickname VARCHAR(20),
+	@dateBirth DATE,
+	
+	
+	@email VARCHAR(100),
+	@password VARCHAR(30),
+	@roleTitle VARCHAR(50) 
+AS
+
+	EXEC AddAccount @email, @password, @roleTitle
+
+	INSERT INTO Visitors(Name, Nickname, Date_of_birth, Account_id)
+	VALUES (
+		@name,
+		@nickname,
+		@dateBirth,
+		(SELECT Id FROM Accounts WHERE Id = (SELECT MAX(Id) FROM Accounts))
+		 
+	)
+
+GO
+
+
+CREATE PROC AddArtist  
+
+	@name VARCHAR(200),
+	@alias VARCHAR(100),
+	@dateBirth DATE,
+
+
+	@email VARCHAR(100),
+	@password VARCHAR(30),
+	@roleTitle VARCHAR(50) 
+AS
+	EXEC AddAccount @email, @password, @roleTitle
+
+	INSERT INTO Artists(Name, Alias, Date_of_birth, Account_id)
+	VALUES(
+		@name, 
+		@alias,
+		@dateBirth,
+		(SELECT Id FROM Accounts WHERE Id = (SELECT MAX(Id) FROM Accounts))
+		)
+GO
+
+
+CREATE PROC AddGroup
+	@title VARCHAR(100),
+	@foundingDate DATE,
+	@numberArtist INT,
+
+	@email VARCHAR(100),
+	@password VARCHAR(30),
+	@roleTitle VARCHAR(50) 
+AS
+	EXEC AddAccount @email, @password, @roleTitle
+	
+	INSERT INTO Groups(Title, Founding_date, Number_of_artist, Account_id)
+	VALUES(
+		@title,
+		@foundingDate, 
+		@numberArtist,
+		(SELECT Id FROM Accounts WHERE Id = (SELECT MAX(Id) FROM Accounts))		
+		)
+GO
+
+CREATE PROC AddSong
+	@title VARCHAR(100),
+	@dateWriting DATE
+AS
+	INSERT INTO Songs(Title, Date_of_writing)
+	VALUES (@title, @dateWriting)
+GO
+
+CREATE PROC AddGenre
+	@title VARCHAR(100)
+AS
+	INSERT INTO Genres(Title) VALUES (@title)
+GO
+
+CREATE PROC AddAlbum
+	@title VARCHAR(100), 
+	@dateWriting DATE
+AS
+	INSERT INTO Albums(Title, Date_of_writing) VALUES (@title, @dateWriting)
+GO
+
+
+CREATE PROC RemoveAccount 
+	@email VARCHAR(100)
+AS
+	DELETE Accounts
+	WHERE Email = @email
+GO
+
+CREATE PROC RemoveSong
+	@title VARCHAR(100)
+AS
+	DELETE Songs
+	WHERE Title = @title
+GO
+
+CREATE PROC RemoveAlbum
+	@title VARCHAR(100)
+AS
+	DELETE Albums
+	WHERE Title = @title
+GO
+
+CREATE PROC RemoveGenre
+	@title VARCHAR(100)
+AS
+	DELETE Genres
+	WHERE Title = @title
+GO
---
+++ TriggerQuery.sql
@@ -0,0 +1,95 @@
+﻿USE MusicService
+GO
+
+
+CREATE TRIGGER Account_INSERT
+ON Accounts
+AFTER INSERT
+AS 
+	INSERT INTO Logs(Entity_Id, Representation)
+	SELECT inserted.Id, 'Account added: ' + inserted.Email + ' Role: ' + Roles.Title
+	FROM inserted, Roles, Accounts
+	WHERE Roles.Id = Accounts.Role_id
+
+GO
+
+
+CREATE TRIGGER Account_DELETE
+ON Accounts
+AFTER DELETE
+AS
+	INSERT Logs(Entity_Id, Representation)
+	SELECT deleted.Id, 'Account deleted: ' + deleted.Email + 'Role: ' + Roles.Title
+	FROM deleted, Roles, Accounts
+	WHERE Roles.Id  = Accounts.Role_id
+
+GO
+CREATE TRIGGER Album_INSERT
+ON Albums
+AFTER INSERT
+AS 
+	INSERT Logs(Entity_Id, Representation)
+	SELECT inserted.Id, 'Album added: ' + inserted.Title 
+	FROM inserted
+
+GO
+CREATE TRIGGER Album_DELETE
+ON Albums
+AFTER DELETE
+AS 
+	INSERT Logs(Entity_Id, Representation)
+	SELECT deleted.Id, 'Album deleted: ' + deleted.Title
+	FROM deleted
+
+GO
+CREATE TRIGGER Songs_INSERT
+ON Songs
+AFTER INSERT
+AS 
+	INSERT Logs(Entity_Id, Representation)
+	SELECT inserted.Id, 'Song added: ' + inserted.Title
+	FROM inserted
+
+GO
+CREATE TRIGGER Songs_DELETE
+ON Songs
+AFTER DELETE
+AS 
+	INSERT Logs(Entity_Id, Representation)
+	SELECT deleted.Id, 'Song deleted: ' + deleted.Title
+	FROM deleted
+
+DROP TRIGGER Songs_DELETE
+
+GO
+CREATE TRIGGER Genres_INSERT
+ON Genres
+AFTER INSERT
+AS 
+	INSERT Logs(Entity_Id, Representation)
+	SELECT inserted.Id, 'Genre added: ' + inserted.Title 
+	FROM inserted
+
+GO
+CREATE TRIGGER Genres_DELETE
+ON Genres
+AFTER DELETE
+AS 
+	INSERT Logs(Entity_Id, Representation)
+	SELECT deleted.Id, 'Genre deleted: ' + deleted.Title 
+	FROM deleted
+
+GO
+INSERT INTO Songs(Title, Date_of_writing) VALUES ('Song', '2020-01-01')
+
+DELETE Songs WHERE Title = 'Song'
+
+SELECT * FROM Songs
+
+SELECT * fROM Logs
+
+SELECT * FROM Albums
+
+EXEC AddAlbum 'Album', '2022-01-01'
+
+DROP TRIGGER Album_INSERT
